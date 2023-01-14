a6ca4d45bfd79e9a70f4a3314ae1200df2093f68
Natthaphong Phengsa <Natthaphong.phengsa@outlook.com>
2023-01-14 22:13:55 +0100

    add films & gallery

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,3 +1,4 @@
+
 document.getElementById('currentYear').innerHTML = new Date().getFullYear();
 
 function rotateToggleIcon(){
@@ -73,104 +74,50 @@ var swiper = new Swiper(".filmSwiper", {
   }, 
 });
 
-var swiper2 = new Swiper(".GallerySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    "@0.00": {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-    "@0.75": {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    "@1.00": {
-      slidesPerView: 3,
-      spaceBetween: 40,
-    },
-    "@1.50": {
-      slidesPerView: 4,
-      spaceBetween: 50,
-    },
-  },
-});
+fetch('./data/video.json')
+.then((response) => response.json())
+.then((json) => fetchVideo(json));
 
-
-window.onload = (event) => {  
-  var playList = [
-    {
-      genre:"Ceremony",
-      url:"https://www.youtube.com/embed/Lnjab17j960",
-      imgSrc:"https://i.ytimg.com/vi/Lnjab17j960/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIFkoSzAP&rs=AOn4CLD2lw-0-PEdCpLgk0NfiN-2Vlc4vw"
-    },
-    {
-      genre:"Ceremony",
-      url:"https://www.youtube.com/embed/RY6HV1pNZmY",
-      imgSrc:"https://i.ytimg.com/vi/RY6HV1pNZmY/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGIoUjAP&rs=AOn4CLDHd7KOE0GhXkKeylp6YhSDx4wBNA"
-    },
-    {
-      genre:"Ceremony",
-      url:"https://www.youtube.com/embed/g_dL86WrF4E",
-      imgSrc:"https://i.ytimg.com/vi/g_dL86WrF4E/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhyIFIoPjAP&rs=AOn4CLAyExS0Hktv0KLABcp1-O2FeBOgsQ"
-    },
-    {
-      genre:"Ceremony",
-      url:"https://www.youtube.com/embed/ISRL6vYV3vE",
-      imgSrc:"https://i.ytimg.com/vi/ISRL6vYV3vE/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhFIGUoNjAP&rs=AOn4CLA9JoXBAbLU2hamAHS6sb4vx-Audg"
-    },
-    {
-      genre:"Wedding",
-      url:"https://www.youtube.com/embed/LRhHvluhbnY",
-      imgSrc:"https://i.ytimg.com/vi/LRhHvluhbnY/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_h2AAvAQigIMCAAQARhZIFQoZTAP&rs=AOn4CLC3GmMOFHOn996y3aAkkKOA5hiQcg"
-    },
-    {
-      genre:"Wedding",
-      url:"https://www.youtube.com/embed/k3fcVIcDCsc",
-      imgSrc:"https://i.ytimg.com/vi/5He0AQzKlcw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBMxlsdXnVeWrn5UtuYFNqDeQ3dBA"
-    },
-    {
-      genre:"Wedding",
-      url:"https://www.youtube.com/embed/5He0AQzKlcw",
-      imgSrc:"https://i.ytimg.com/vi/k3fcVIcDCsc/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB1AaAAuADigIMCAAQARhlIGAoWjAP&rs=AOn4CLDN_NRJcDIch1DzJmoJKSwpEitA4Q"
-    },
-  ];
-
+function fetchVideo(playList){      
   const element = document.getElementById('youtubeplaylist');
   var genreCount = 0;
 
   for (let index = 0; index < playList.length; index++) {          
     const video = playList[index];
 
+    const videoElement = document.createElement("a");
+    const innerElement = document.createElement("div");
+    const overlay = document.createElement("div");
+    const text = document.createElement("div");
     const image = document.createElement("img");
-    image.setAttribute('src', `${video.imgSrc}`);
-    image.setAttribute('alt', `${video.url}`);
-    image.setAttribute('class','w-100 rounded-4 projects');
-
     const span = document.createElement("span");
+    const arrow = document.createElement("i");
+    const playIcon = document.createElement("i");
+
+    playIcon.setAttribute('class','innerText position-absolute text-primary m-0 bi bi-play-circle');
+
+    image.setAttribute('src', `${video.poster}`);
+    image.setAttribute('alt', `${video.link}`);
+    image.setAttribute('class','w-100 rounded-4');
+
+    overlay.setAttribute('class','videoInnerOverlay rounded-4');    
+    overlay.append(playIcon);  
+
+    innerElement.setAttribute('class','projects position-relative rounded-4');
+    innerElement.append(image);    
+    innerElement.append(overlay); 
+
     span.style.fontFamily = "Noto Sans,Arial,Helvetica,sans-serif";
     span.style.color = "#fff";
     span.style.fontSize ="28px";
+    span.style.marginBottom = "6px";
 
-    const arrow = document.createElement("i");
     arrow.setAttribute('class','bi bi-caret-right-fill');
     arrow.style.fontSize = "16px";
 
-    const videoElement = document.createElement("a");
     videoElement.setAttribute('class','col-lg-3 pb-4 bg-transparent border-0');
-    videoElement.appendChild(image);
-    videoElement.href = `${video.url}`;
+    videoElement.append(innerElement);
+    videoElement.href = `${video.link}`;
 
     if(video.genre == "Ceremony"){
       if(genreCount == 0){
@@ -191,5 +138,4 @@ window.onload = (event) => {
       element.append(videoElement);
     }
   }
-  console.log(playList)
 };
